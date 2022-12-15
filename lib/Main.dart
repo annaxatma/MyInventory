3d6e87a667ad2485fda7b6d5f8a56401d17f0101
MitchyMichael <84000679+MitchyMichael@users.noreply.github.com>
2022-12-15 08:25:31 +0700

    login register design done

--- lib/Main.dart
+++ lib/Main.dart
@@ -12,65 +12,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // Removing the "debug" text.
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
       initialRoute: '/',
       routes: {
         // '/': (context) => Home(),
-        '/': (context) => Login(),
-        Home.routeName: (context) => Home(),
-        Profile.routeName: (context) => Profile(),
         // Login.routeName: (context) => Login(),
-        Register.routeName: (context) => Register(),
+
+        '/': (context) => const Login(),
+        Home.routeName: (context) => const Home(),
+        Profile.routeName: (context) => const Profile(),
+        //Register.routeName: (context) => const Register(),
       },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
--- lib/view/pages/Register.dart
+++ lib/view/pages/Register.dart
@@ -1,111 +1,157 @@
-part of 'Pages.dart';
+part of "Pages.dart";
 
 class Register extends StatefulWidget {
   const Register({Key? key}) : super(key: key);
 
-  static const String routeName = "/Register";
-
   @override
-  State<Register> createState() => _RegisterState();
+  _RegisterState createState() => _RegisterState();
 }
 
 class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            body: Column(
-              // 2nd layer to contain everything else :v
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(
-                  height: 180,
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Icon(
+                      Icons.arrow_back,
+                      color: Color.fromARGB(255, 145, 145, 145),
+                    ),
+                  ),
                 ),
-                Flexible(flex: 1, child: Row(
-                  // I know it's a lot but trust me, it's just for the "sign in" text :v
-                  children: [
-                    Flexible(
-                        child: Container(
-                          margin: const EdgeInsets.only(left: 30),
-                          child: TextButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                                },
-                              child: Text.rich(
-                                TextSpan(
-                                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.transparent, letterSpacing: 4),
-                                  children: <TextSpan>[
-                                    TextSpan(
-                                        text: 'Sign',
-                                        style: TextStyle(
-                                          decoration: TextDecoration.underline,
-                                          shadows: [Shadow(color: Color.fromARGB(
-                                              255, 124, 150, 109), offset: Offset(0, -10))],
-                                        )
-                                    ),
-                                    TextSpan(
-                                        text: ' In',
-                                        style: TextStyle(
-                                          decoration: TextDecoration.underline,
-                                          shadows: [Shadow(color: Color.fromARGB(
-                                              255, 124, 150, 109), offset: Offset(0, -10))],
-                                        )
-                                    )
-                                    // can add more TextSpans here...
-                                  ],
-                                ),
-                              )
-                          ),
-                        )
+                SizedBox(
+                  height: 32,
+                ),
+                // This is the login text.
+                Container(
+                  width: double.infinity,
+                  child: Text(
+                    "Create Account",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 40, 107, 53),
+                        fontWeight: FontWeight.w900,
+                        fontSize: 38),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+
+                SizedBox(
+                  height: 32,
+                ),
+                TextFormField(
+                  style: const TextStyle(
+                      color: Color.fromARGB(255, 145, 145, 145), fontSize: 14),
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    labelText: "Name",
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  style: const TextStyle(
+                      color: Color.fromARGB(255, 145, 145, 145), fontSize: 14),
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    labelText: "Email",
+                    prefixIcon: Icon(Icons.email),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  // Note: Pass input
+                  style: const TextStyle(
+                      color: Color.fromARGB(255, 145, 145, 145), fontSize: 14),
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: const InputDecoration(
+                    labelText: "Password",
+                    prefixIcon: Icon(Icons.key),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+
+                TextFormField(
+                  // Note: Pass input
+                  style: const TextStyle(
+                      color: Color.fromARGB(255, 145, 145, 145), fontSize: 14),
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: const InputDecoration(
+                    labelText: "Confirm Password",
+                    prefixIcon: Icon(Icons.key),
+                  ),
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => const Login()),
+                      );
+                    },
+                    child: Text('SIGN UP'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 40, 107, 53),
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(50),
+                      ),
                     ),
-                    // This is for the "sign up" button
-                    Flexible(
-                        child: Container(
-                          child: TextButton(
-                              onPressed: () {  },
-                              child: Text.rich(
-                                TextSpan(
-                                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.transparent, letterSpacing: 4),
-                                  children: <TextSpan>[
-                                    TextSpan(
-                                        text: 'Si',
-                                        style: TextStyle(
-                                          decoration: TextDecoration.underline,
-                                          decorationColor: Color.fromARGB(
-                                              255, 58, 177, 73),
-                                          decorationThickness: 3,
-                                          shadows: [Shadow(color: Color.fromARGB(
-                                              255, 38, 107, 53), offset: Offset(0, -10))],
-                                        )
-                                    ),
-                                    TextSpan(
-                                        text: 'gn',
-                                        style: TextStyle(
-                                          decoration: TextDecoration.underline,
-                                          shadows: [Shadow(color: Color.fromARGB(
-                                              255, 38, 107, 53), offset: Offset(0, -10))],
-                                        )
-                                    ),
-                                    TextSpan(
-                                        text: ' Up',
-                                        style: TextStyle(
-                                          decoration: TextDecoration.underline,
-                                          shadows: [Shadow(color: Color.fromARGB(
-                                              255, 38, 107, 53), offset: Offset(0, -10))],
-                                        )
-                                    )
-                                    // can add more TextSpans here...
-                                  ],
-                                ),
-                              )
-                          ),
-                        )
-                    ),
-                  ],
-                )),
+                  ),
+                ),
               ],
-            )
-        )
+            ),
+          ),
+          Align(
+            alignment: const Alignment(0, 0.9),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Login()),
+                );
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account?",
+                    style: TextStyle(color: Color.fromARGB(255, 145, 145, 145)),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    "Sign In",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 40, 107, 53),
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
