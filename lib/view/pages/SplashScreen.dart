b6761977f6fa90eaa51c6cc5e826517dd30286dd
Angelo Kusuma <84068505+Akusuma05@users.noreply.github.com>
2022-12-22 22:30:58 +0700

    Revert "Revert "Merge branch 'master' into Angelo""
    This reverts commit 2bfcd45676e36a10afda92c3776d9608699473d2.

--- lib/view/pages/SplashScreen.dart
+++ lib/view/pages/SplashScreen.dart
@@ -1,10 +1,33 @@
 part of 'Pages.dart';
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({ Key? key }) : super(key: key);
+
+  static const String routeName = "/SplashScreen";
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    new Future.delayed(
+        const Duration(seconds: 3),
+        () => Navigator.pushNamed(
+              context,
+              Login.routeName
+            ));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Image.asset("assets/logo/LogoHijau.png", width: 150,),
+      ),
+    );
   }
 }
